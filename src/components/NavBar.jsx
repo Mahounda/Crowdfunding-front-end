fdd52ece1a78e040cae5be5ce030f54a97c15ef0
Mahounda Poinsonnet <roromahounda@gmail.com>
2026-02-15 21:26:39 +1000

    navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 import { Link } from "react-router-dom";
 import "../components/NavBar.css";
+
 function NavBar() {
   return (
-    <div>
-      <nav>
+    <nav className="navbar">
+      <Link to="/" className="nav-logo">
+        2gethR
+      </Link>
+
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/login">Log In</Link>
         <Link to="/signup">Sign Up</Link>
         <Link to="/fundraiser/create">Create Fundraiser</Link>
         <Link to="/contact">Contact</Link>
         <Link to="/about">About</Link>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
