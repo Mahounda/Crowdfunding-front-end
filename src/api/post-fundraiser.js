a08fcebf17ea196251afed5ceb1dcd0fe52bbe90
Mahounda Poinsonnet <roromahounda@gmail.com>
2026-02-18 22:04:42 +1000

    Restore post-fundraiser.js

---
+++ src/api/post-fundraiser.js
@@ -0,0 +1,27 @@
+async function postFundraiser(formData, token) {
+  const url = `${import.meta.env.VITE_API_URL}/fundraisers/`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token ${token}`,
+    },
+    body: JSON.stringify(formData),
+  });
+
+  if (!response.ok) {
+    const fallbackError = `Error creating fundraiser`;
+
+    const data = await response.json().catch(() => {
+      throw new Error(fallbackError);
+    });
+
+    const errorMessage = data?.detail ?? fallbackError;
+    throw new Error(errorMessage);
+  }
+
+  return await response.json();
+}
+
+export default postFundraiser;
