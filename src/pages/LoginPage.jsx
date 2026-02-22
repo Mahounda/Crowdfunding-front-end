e89f0a96f5a62baea5ba05698911c8fcfcbb1459
Mahounda Poinsonnet <roromahounda@gmail.com>
2026-02-22 11:42:41 +1000

    update login page when user log in out to create fundraiser

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,12 @@
+import { useLocation } from "react-router-dom";
 import LoginForm from "../components/LoginForm";
+
 function LoginPage() {
-  return <LoginForm />;
+  const location = useLocation();
+  const from = location.state?.from || "/";
+
+  return <LoginForm redirectTo={from} />;
 }
 
 export default LoginPage;
+
